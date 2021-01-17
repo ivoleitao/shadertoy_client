14e1073a9de99e5550bdd6960d8927ccab332bb3
Ivo Leitão <ivo.leitao@sky.uk>
2021-01-17 10:11:27 +0000

    Trying to understand the cause of the unit tests errors only on github actions

--- test/site/site_client_test.dart
+++ test/site/site_client_test.dart
@@ -1263,6 +1263,29 @@ void main() {
       expect(sr.error, isNull);
       expect(sr.comments, isNotNull);
       expect(sr.comments, isNotEmpty);
+
+      expect(sr.comments[0].id, 'XlGcRK');
+      expect(sr.comments[0].userId, 'wosztal15');
+      expect(sr.comments[0].picture, '/media/users/wosztal15/profile.jpeg');
+      expect(sr.comments[0].date, DateTime(2020, 09, 11, 11, 37, 32));
+      expect(sr.comments[0].text,
+          '\nI have to admit that it makes an amazing impression!');
+      expect(sr.comments[0].hidden, false);
+
+      expect(sr.comments[1].id, '4lGyzG');
+      expect(sr.comments[1].userId, 'Cubex');
+      expect(sr.comments[1].picture, '/media/users/Cubex/profile.png');
+      expect(sr.comments[1].date, DateTime(2020, 09, 7, 16, 47, 38));
+      expect(sr.comments[1].text, 'Woobly moobly, it\'s amazing!');
+      expect(sr.comments[1].hidden, false);
+
+      expect(sr.comments[2].id, 'Xd2GW1');
+      expect(sr.comments[2].userId, 'iq');
+      expect(sr.comments[2].picture, '/media/users/iq/profile.png');
+      expect(sr.comments[2].date, DateTime(2014, 03, 17, 16, 35, 55));
+      expect(sr.comments[2].text, 'Oh, I love it!');
+      expect(sr.comments[2].hidden, false);
+
       expect(
           sr,
           FindCommentsResponse(comments: [
